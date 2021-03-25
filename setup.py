de664719c9736aae91962f0bcd62ed19cea7071b
Teddy Hartanto <teddyhartanto96@gmail.com>
2021-03-25 17:07:16 +0800

    Don't pin the versions of the dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="flasksaml",
-    version="0.1.0",
+    version="0.2.0",
     description="A Flask wrapper that implements SAML Service Provider functionalities",
     long_description=long_description,
     long_description_content_type="text/markdown",
